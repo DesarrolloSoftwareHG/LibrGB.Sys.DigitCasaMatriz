609ef7ccb67df1794254e677e686a6a2ead3fd1c
Osmany Lopez <osmany203lopez@gmail.com>
2023-08-20 15:27:29 -0600

    Se agrego la carpeta de proveedor y su diseño de la pagina wpf.

---
+++ LibrGB.Sys.DigitCasaMatriz.UI/Proveedor/Buscar-VerProveedor.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LibrGB.Sys.DigitCasaMatriz.UI.Proveedor
+{
+    /// <summary>
+    /// Lógica de interacción para Buscar_VerProveedor.xaml
+    /// </summary>
+    public partial class Buscar_VerProveedor : Page
+    {
+        public Buscar_VerProveedor()
+        {
+            InitializeComponent();
+        }
+    }
+}
