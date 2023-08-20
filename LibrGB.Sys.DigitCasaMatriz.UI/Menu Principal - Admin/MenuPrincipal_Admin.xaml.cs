0f782dd0295b403814b21c0d647524897d240fc5
Osmany Lopez <osmany203lopez@gmail.com>
2023-08-20 15:27:13 -0600

    Se agrego la carpeta de menu principal y su diseño de la ventana wpf.

---
+++ LibrGB.Sys.DigitCasaMatriz.UI/Menu Principal - Admin/MenuPrincipal_Admin.xaml.cs
@@ -0,0 +1,28 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LibrGB.Sys.DigitCasaMatriz.UI.Menu_Principal___Admin
+{
+    /// <summary>
+    /// Lógica de interacción para MenuPrincipal_Admin.xaml
+    /// </summary>
+    public partial class MenuPrincipal_Admin : MetroWindow
+    {
+        public MenuPrincipal_Admin()
+        {
+            InitializeComponent();
+        }
+    }
+}
