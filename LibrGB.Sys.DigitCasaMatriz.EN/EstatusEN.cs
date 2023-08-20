673e79d23224ea0dd80042759e846d3de7125e32
Hector Garcia <hg425823@gmail.com>
2023-08-20 13:21:01 -0600

    Incorporacion de la clase con codigo funcional de EstadoCivil , Estatus y Genero

---
+++ LibrGB.Sys.DigitCasaMatriz.EN/EstatusEN.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrGB.Sys.DigitCasaMatriz.EN
+{
+    public class EstatusEN
+    {
+        public int Id { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Descripcion { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+        public DateTime FechaModificacion { get; set; }
+    }
+}
