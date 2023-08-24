d13590c941e839fb02ef2bd9d4db97533901925d
Hector Garcia <hg425823@gmail.com>
2023-08-24 12:07:49 -0600

    asignacion de nombre al boton de ver en la vista Buscar-VerCategoriaCU

--- LibrGB.Sys.DigitCasaMatriz.UI/Categoria/Buscar-VerCategoriaCU.xaml.cs
+++ LibrGB.Sys.DigitCasaMatriz.UI/Categoria/Buscar-VerCategoriaCU.xaml.cs
@@ -130,5 +130,11 @@ namespace LibrGB.Sys.DigitCasaMatriz.UI.Categoria
         {
             this.Visibility = Visibility.Collapsed;
         }
+
+        private void btnRecargarCategoria_Click(object sender, RoutedEventArgs e)
+        {
+            ActualizarDataGrid();
+            //Llamamos el metodo ActualizarDataGrid para actualizar el DGV al precionarlo
+        }
     }
 }
