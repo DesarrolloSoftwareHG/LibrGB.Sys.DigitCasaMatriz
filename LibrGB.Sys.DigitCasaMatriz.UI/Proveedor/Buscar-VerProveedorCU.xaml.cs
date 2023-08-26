939db0a3b75e1c02dc3359f5ef03c4ebbae58f6e
Hector Garcia <hg425823@gmail.com>
2023-08-25 22:20:02 -0600

    Migracion de vista a _MantenimientoProveedor antes llamada AgregarProveedor

--- LibrGB.Sys.DigitCasaMatriz.UI/Proveedor/Buscar-VerProveedorCU.xaml.cs
+++ LibrGB.Sys.DigitCasaMatriz.UI/Proveedor/Buscar-VerProveedorCU.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LibrGB.Sys.DigitCasaMatriz.BL.Catalogo_BL;
+using LibrGB.Sys.DigitCasaMatriz.UI.Categoria;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static LibrGB.Sys.DigitCasaMatriz.EN.Acciones;
 
 namespace LibrGB.Sys.DigitCasaMatriz.UI.Proveedor
 {
@@ -23,12 +26,34 @@ namespace LibrGB.Sys.DigitCasaMatriz.UI.Proveedor
         public Buscar_VerProveedorCU()
         {
             InitializeComponent();
+
+            ActualizarDataGrid();
+            // Llamamos el Metodo ActualizarGrid para que se ejecute al nomas inicie el Control de Usuario
         }
+
+        public void ActualizarDataGrid()
+        {
+            // Se establece la fuente de datos del DataGridView (dgvMostrar_Categorias) como nula para limpiar los datos actuales.
+            dgvMostrar_Proveedor.ItemsSource = null;
+
+            // Se asigna la fuente de datos del DataGridView (dgvMostrar_Categorias) con la lista de categorías obtenidas a través de ObjCategoriaBL.ObtenerCategoria().
+            dgvMostrar_Proveedor.ItemsSource = ObjProveedorBL.ObtenerProveedor();
+
+        }
+
+        ProveedorBL ObjProveedorBL = new ProveedorBL();
+        // Crea una instancia de la clase CategoriaBL y la asigna a la variable ObjCategoriaBL.
 
         private void btnAgregarProveedor_Click(object sender, RoutedEventArgs e)
         {
-            _ProveedorAgregar AgregarProveedor = new _ProveedorAgregar();
-            AgregarProveedor.Show();
+            // Se define la acción que se realizará, en este caso, se establece la acción como "Crear".
+            var accion = (byte)AccionEnum.Crear;
+
+            // Se crea una instancia del formulario _CategoriaAgregar pasando null como parámetro y la acción definida.
+            _MantenimientoProveedor AgregFormulario = new _MantenimientoCategoria(null, accion);
+
+            // Se muestra el formulario recién creado.
+            AgregFormulario.Show();
         }
 
         private void btnCerrar_Click(object sender, RoutedEventArgs e)
