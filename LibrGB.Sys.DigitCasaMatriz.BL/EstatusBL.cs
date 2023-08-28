e70f73cc3a6f468ce9ab05e61321b026fdd66fc7
Hector Garcia <hg425823@gmail.com>
2023-08-27 22:24:50 -0600

    Actualizcion de Documentacion de la Clase EstatusBL

--- LibrGB.Sys.DigitCasaMatriz.BL/EstatusBL.cs
+++ LibrGB.Sys.DigitCasaMatriz.BL/EstatusBL.cs
@@ -13,25 +13,17 @@ namespace LibrGB.Sys.DigitCasaMatriz.BL
         EstatusDAL ObjEstatusDAL = new EstatusDAL();
         // Se crea una instancia del objeto EstatusDAL y se asigna a la variable ObjEstatusDAL
 
+        // Obtiene una lista de todos los estatus disponibles en la base de datos.
         public List<EstatusEN> ObtenerEstatus()
         {
             return ObjEstatusDAL.ObtenerEstatus();
-            // Llama al método "ObtenerEstatus" de ObjEstatusDAL y devuelve el resultado.
-
-            // Comentario adicional:
-            // El método "ObtenerEstatus" se encarga de obtener una lista de objetos EstatusEN de la base de datos.
         }
 
+        // Obtiene los detalles de un estatus específico utilizando su identificador.
         public EstatusEN ObtenerEstatusPorId(int? pId)
         {
             return ObjEstatusDAL.ObtenerEstatusPorId(pId);
-            // Llama al método "ObtenerEstatusPorId" de ObjEstatusDAL pasando el parámetro pId y devuelve el resultado.
-
-            // Comentario adicional:
-            // El método "ObtenerEstatusPorId" se encarga de obtener una unidad de medida por su Id desde la base de datos.
-            // Realiza la lógica necesaria para buscar y obtener el estatus correspondiente al Id proporcionado
-            // y devuelve un objeto EstatusEN con la información del estatus encontrado. El parámetro pId es
-            // de tipo int? (nullable), lo que significa que puede ser nulo.
         }
+
     }
 }
